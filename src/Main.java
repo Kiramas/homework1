6e281ce6072ea61408fd626c7ad46314e5c5ae44
Kira Kolmakova <lyraoctavia@gmail.com>
2023-02-02 19:44:05 +0500

    branch longer

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] number = {200, 300, 400};
+        long[] number = {200, 300, 400};
         SalesManager salesManager = new SalesManager(number);
         System.out.println("Максимум на сегодня " + salesManager.max());
     }
