686ff9b5353346dd65c8cdefe395fa3b80439f79
Kira Kolmakova <lyraoctavia@gmail.com>
2023-02-03 03:15:23 +0500

    branch stat

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,24 @@ public class Main {
         long[] number = {200, 300, 400};
         SalesManager salesManager = new SalesManager(number);
         System.out.println("Максимум на сегодня " + salesManager.max());
+
+        int maxNum = number[0];
+        int minNum = number[0];
+
+        for (int i = 1; i < number.length; i++) {
+            maxNum = Math.max(maxNum, number[i]);
+        }
+
+        for (int i = 1; i < number.length; i++) {
+            minNum = Math.min(minNum, number[i]);
+        }
+        int sum = 0;
+        for (int i = 0; i < number.length; i++) {
+            sum += number[i];
+        }
+
+        int res;
+        res = (sum - maxNum - minNum) / number.length;
+        System.out.println("Обрезанное среднее: " + res);
     }
 }
